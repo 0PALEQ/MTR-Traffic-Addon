9a99bd5800d079def6458ad92565332447ea94b9
0PALEQ <opaleq@icloud.com>
2026-05-17 22:07:28 +0200

    Initial commit

---
+++ src/client/java/com/cookiecraftmods/mta/client/MTRTrafficAddonClient.java
@@ -0,0 +1,10 @@
+package com.cookiecraftmods.mta.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MTRTrafficAddonClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
