c562a5e56ff776380f135f08292536455efe4f40
opaleq <131912272+0PALEQ@users.noreply.github.com>
2026-08-13 19:18:40 +0200

    removed unused classes and methods leftovers from the alpha version

--- src/main/java/com/cookiecraftmods/mta/traffic/point/block/TrafficPointBlock.java
+++
@@ -1,9 +0,0 @@
-package com.cookiecraftmods.mta.traffic.point.block;
-
-import com.cookiecraftmods.mta.traffic.point.TrafficPointType;
-
-public interface TrafficPointBlock {
-	TrafficPointType getTrafficPointType();
-}
-
-//
